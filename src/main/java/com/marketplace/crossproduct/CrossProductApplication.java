10e92760d357aa073505a8ce9ed943867f7f371d
Pereira <carlos.pereira@tymit.com>
2025-04-05 14:05:00 +0200

    Initial commit

---
+++ src/main/java/com/marketplace/crossproduct/CrossProductApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.crossproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketplace/crossproduct/CrossProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.crossproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
