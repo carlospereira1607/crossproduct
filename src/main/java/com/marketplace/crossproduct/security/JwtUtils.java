1d32e9150d8eb659e421e8f40ab4df19496dd6a7
Pereira <carlos.pereira@tymit.com>
2025-04-05 22:29:26 +0200

    Refactor

--- src/main/java/com/marketplace/crossproduct/security/JwtUtils.java
+++ src/main/java/com/marketplace/crossproduct/security/JwtUtils.java
@@ -1,11 +1,8 @@
 package com.marketplace.crossproduct.security;
 
 import com.marketplace.crossproduct.core.model.User;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
@@ -56,16 +53,8 @@ public class JwtUtils {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
-        } catch (SecurityException e) {
-            log.error("Invalid JWT signature: {}", e.getMessage());
-        } catch (MalformedJwtException e) {
-            log.error("Invalid JWT token: {}", e.getMessage());
-        } catch (ExpiredJwtException e) {
-            log.error("JWT token is expired: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            log.error("JWT token is unsupported: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            log.error("JWT claims string is empty: {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Invalid JWT: {}", e.getMessage());
         }
         return false;
     }
