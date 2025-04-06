9220c5e396928e33819622728567bcdd5a60a964
Pereira <carlos.pereira@tymit.com>
2025-04-06 10:19:47 +0200

    Refactor query

--- src/main/java/com/marketplace/crossproduct/outgoing/adapter/db/repository/ProductEntityRepository.java
+++ src/main/java/com/marketplace/crossproduct/outgoing/adapter/db/repository/ProductEntityRepository.java
@@ -9,10 +9,9 @@ import java.util.Set;
 
 public interface ProductEntityRepository extends JpaRepository<ProductEntity, Long>  {
 
-    @Query("SELECT p FROM Product p " +
-            "JOIN p.attributes a " +
-            "JOIN a.definition d " +
-            "WHERE d.portal.id = :portalId")
+    @Query(value = "SELECT p.* FROM product p INNER JOIN attribute a ON a.product_id = p.id " +
+            "INNER JOIN attribute_definition ad ON ad.id = a.attribute_definition_id " +
+            "WHERE ad.portal_id = :portalId", nativeQuery = true)
     Set<ProductEntity> findAllByPortalId(@Param("portalId") Long portalId);
 
 }
