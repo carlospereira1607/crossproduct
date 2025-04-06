cffe5c06fac6a97ed4c8c89461bc8930862529ff
Pereira <carlos.pereira@tymit.com>
2025-04-06 18:19:34 +0200

    Fix DB query

--- src/main/java/com/marketplace/crossproduct/outgoing/adapter/db/repository/ProductEntityRepository.java
+++ src/main/java/com/marketplace/crossproduct/outgoing/adapter/db/repository/ProductEntityRepository.java
@@ -5,14 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ProductEntityRepository extends JpaRepository<ProductEntity, Long> {
 
     @Query(value = """ 
-                SELECT p.* FROM product p INNER JOIN attribute_value av ON av.product_id = p.id
-                        INNER JOIN attribute_definition ad ON ad.id = av.definition_id
-                WHERE ad.portal_id = :portalId""", nativeQuery = true)
-    Set<ProductEntity> findAllByPortalId(@Param("portalId") Long portalId);
+                SELECT p.*
+                FROM product p
+                INNER JOIN attribute_value av ON av.product_id = p.id
+                INNER JOIN attribute_definition ad ON ad.id = av.definition_id
+                WHERE av.portal_id = :portalId
+            """, nativeQuery = true)
+    List<ProductEntity> findAllByPortalId(@Param("portalId") Long portalId);
 
 }
