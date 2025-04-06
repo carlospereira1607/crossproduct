506ec98ca1d9e39cfb4f60de54bfe70bb933e35f
Pereira <carlos.pereira@tymit.com>
2025-04-06 08:16:03 +0200

    Add repository methods

--- src/main/java/com/marketplace/crossproduct/core/port/db/AttributeDefinitionPortRepository.java
+++ src/main/java/com/marketplace/crossproduct/core/port/db/AttributeDefinitionPortRepository.java
@@ -1,4 +1,15 @@
 package com.marketplace.crossproduct.core.port.db;
 
+import com.marketplace.crossproduct.core.model.AttributeDefinition;
+
+import java.util.Optional;
+
 public interface AttributeDefinitionPortRepository {
+
+    AttributeDefinition save(final AttributeDefinition attributeDefinition);
+
+    Optional<AttributeDefinition> findByPortalIdAndName(final Long portalId, final String name);
+
+    Optional<AttributeDefinition> findById(final Long id);
+
 }
--- src/main/java/com/marketplace/crossproduct/core/port/db/AttributeValuePortRepository.java
+++ src/main/java/com/marketplace/crossproduct/core/port/db/AttributeValuePortRepository.java
@@ -1,4 +1,13 @@
 package com.marketplace.crossproduct.core.port.db;
 
+import com.marketplace.crossproduct.core.model.AttributeValue;
+
+import java.util.Optional;
+
 public interface AttributeValuePortRepository {
+
+    Optional<AttributeValue> findById(final Long id);
+
+    AttributeValue save(final AttributeValue attributeValue);
+
 }
